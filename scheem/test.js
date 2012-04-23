d8af79a415b8d2d5855dd80f99bbf008b8295a60
lucastx <lucas@lucasteixeira.com>
2012-04-22 22:49:20 -0300

    Refactored tests; wrote a test for quoting

--- scheem/test.js
+++ scheem/test.js
@@ -1,26 +1,12 @@
 var assert = require('assert');
 var scheem = require('./scheem.js');
 
-tests = [
-    {
-        expr: '(* n (factorial (- n 1)))',
-        result: ["*", "n", ["factorial", ["-", "n", "1"]]]
-    },
+var factorial_scheem = '(* n (factorial (- n 1)))';
+var factorial_result = ["*", "n", ["factorial", ["-", "n", "1"]]];
+assert.deepEqual(scheem.parse(factorial_scheem), factorial_result);
 
-    {
-        expr: '(+ 1 (* x 3))',
-        result: ["+", "1", ["*", "x", "3"]]
-    }
-];
+var quoted1 = "(quote (1 2 3))";
+var quoted2 = "'(1 2 3)";
+assert.deepEqual(scheem.parse(quoted1), scheem.parse(quoted2));
 
-tests.forEach(function(test) {
-    process.stdout.write("Testing: " + test.expr + "... ", 'utf-8');
-    try {
-        assert.deepEqual(scheem.parse(test.expr), test.result);
-        console.log("success!");
-    } catch(err) {
-        console.log("ERROR:");
-        console.log(err);
-        process.exit(-1);
-    }
-});
+console.log("success!");
